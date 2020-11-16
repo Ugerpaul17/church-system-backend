235fa2b1888290205b2885412bb1a96e1a75d789
ugerpaul17 <ugerpaul5@gmail.com>
2020-11-16 19:17:19 +0300

    format authController, errorController, userModel

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,89 +1,96 @@
-const {promisify} = require('util');
-const jwt = require('jsonwebtoken');
-const User = require('./../models/userModel');
+const { promisify } = require("util");
+const jwt = require("jsonwebtoken");
+const User = require("./../models/userModel");
 const catchAsync = require("./../utile/catchAsync");
-const AppError = require('./../utile/appError');
+const AppError = require("./../utile/appError");
 
-const signToken = id => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    });
-}
-
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
-    });
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-    const token = signToken(newUser._id);
- 
-    res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-            user: newUser
-        }
-    });
+  const token = signToken(newUser._id);
+
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-    const { email, password } = req.body;
-    
-    if (email || password) {
-        next(new AppError('Please provide email and password!', 400));
-    }
+  const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).Select('+password');
+  if (email || password) {
+    next(new AppError("Please provide email and password!", 400));
+  }
 
-    if (!user || !(user.correctPassword(password, user.password))) {
-        return next(new AppError('Incorrect email or password', 401));
-    }
-    
-    const token = signToken(user._id);
+  const user = await User.findOne({ email }).Select("+password");
 
-    res.status(200).json({
-        status: 'success',
-        token
-    });
+  if (!user || !user.correctPassword(password, user.password)) {
+    return next(new AppError("Incorrect email or password", 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
 });
-
 
 exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        
-         token = req.headers.authorization.split(' ') [1]
-    }
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access", 401)
+    );
+  }
 
-    if (!token) {
-        return next(new AppError('You are not logged in! Please log in to get access', 401));
-    }
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-    
-    const freshUser = await User.findById(decoded.id);
+  const freshUser = await User.findById(decoded.id);
 
-    if (!freshUser) {
-        return next(new AppError('The user belonging to this token does no longer exit. ', 401));
-    }
+  if (!freshUser) {
+    return next(
+      new AppError(
+        "The user belonging to this token does no longer exit. ",
+        401
+      )
+    );
+  }
 
-    res.body = freshUser;
-        
+  res.body = freshUser;
 
-        next();
+  next();
 });
- 
-exports.restrictTo = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return next(new AppError('You do not have permission to perform this action', 403));
-        }
 
-        next();
-    };
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+
+    next();
+  };
 };
--- controllers/errorController.js
+++ controllers/errorController.js
@@ -64,7 +64,7 @@ module.exports = (err, req, res, next) => {
     if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
     if (error.name === "JsonWebTokenError") error = handleJWTError();
-    if (error.naem === "TokenExpiredError") error = handleJWTExpiredError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
     sendErrorProd(error, res);
   }
