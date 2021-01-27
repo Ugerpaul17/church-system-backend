ba5ce1ffef2f5c72b633ae7a90d2c4571337c09a
ugerpaul17 <ugerpaul5@gmail.com>
2021-01-27 16:04:24 +0300

    swagger fo Api documemtation

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const AppError = require("./utile/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -14,6 +15,41 @@ const userRouter = require("./routes/userRoutes");
 const attendanceRouter = require("./routes/attendanceRoutes");
 const financeRouter = require("./routes/financeRoutes");
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogRocket Express API with Swagger",
+      version: "0.1.0",
+      description:
+        "This is a simple CRUD API application made with Express and documented with Swagger",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Development server",
+      },
+    ],
+  },
+  apis: [
+    "./routes/membersRoutes.js",
+    "./routes/attendanceRoutes.js",
+    "./routes/financeRoutes.js",
+    "./routes/userRoutes.js",
+  ],
+};
+
+const specs = swaggerJsdoc(options);
+
 const app = express();
 
 //middleware
@@ -21,16 +57,18 @@ app.use(helmet());
 
 app.use(bodyParser.json());
 
-//Data sanitization against NoSQL query injection 
+//Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
 
 //Data sanitization against XSS
 app.use(xss());
 
-//prevent parameter pollution 
+//prevent parameter pollution
 app.use(hpp());
 
 app.use(cors());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
--- routes/membersRoutes.js
+++ routes/membersRoutes.js
@@ -1,3 +1,103 @@
+/**
+ * @swagger
+ * /api/v1/members:
+ *   get:
+ *     summary: Retrieve a list of Members.
+ *     description: Retrieve a list of members from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of members.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The member ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The member's name.
+ *                         example: Uger Paul
+ */
+
+ /**
+ * @swagger
+ * /api/v1/members:
+ *   post:
+ *     summary: Create a member.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The user's name.
+ *                 example: Leanne Graham
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The member ID.
+ *                       example: 0
+ *                     name:
+ *                       type: string
+ *                       description: The member's name.
+ *                       example: Uger Paul
+*/
+
+ /**
+ * @swagger
+ * /api/v1/members/{id}:
+ *   get:
+ *     summary: Retrieve a single member.
+ *     description: Retrieve a single member. Can be used to populate a member profile when prototyping or testing an API.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the member to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single member.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: integer
+ *                       description: The member ID.
+ *                       example: 0
+ *                     name:
+ *                       type: string
+ *                       description: The member's name.
+ *                       example: Uger Paul
+*/
+
 const express = require("express");
 const memberController = require("./../controllers/membersController");
 const authController = require("./../controllers/authController");
@@ -6,7 +106,8 @@ const router = express.Router();
 
 router
   .route("/")
-  .get(authController.protect, memberController.getAllMembers)
+  // .get(authController.protect, memberController.getAllMembers)
+  .get(memberController.getAllMembers)
   .post(memberController.createMember);
 
 router
